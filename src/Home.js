9fd539659a625ec88621e7693d383325481a8e86
Brit Wallace <britwallace888@gmail.com>
2022-06-21 20:17:16 -0700

    Add blogs and styles

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'Lorem ipsum...', author: 'Bird', id: 1},
+    { title: 'Party', body: 'Lorem ipsum...', author: 'Tiger', id: 2},
+    { title: 'Tips', body: 'Lorem ipsum...', author: 'Dog', id: 3},
+  ]);
+
   return (  
-    <div className="home">
-    <h2>Homepage</h2>
+    <div className='home'>
+      {blogs.map((blog) => (
+        <div className='blog-preview' key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+
+    
     </div>
   );
 }
- 
+
 export default Home;
