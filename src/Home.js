687bcbc038054afbc90e961e8143c908d2dcf6e0
Brit Wallace <britwallace888@gmail.com>
2022-07-06 17:27:35 -0700

    Make custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,22 @@
 import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
+  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs")
 
-  const handleDelete = (id) => {
-    const newBlog = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlog);
-  }
+  // const handleDelete = (id) => {
+  //   const newBlog = blogs.filter(blog => blog.id !== id);
+  //   // setBlogs(newBlog);
+  // }
 
-  useEffect (() => {
-    fetch('http://localhost:8000/blogs') 
-        .then(res => { 
-          return res.json();
-        }) 
-        .then((data) => { 
-          setBlogs(data)
-          setIsPending(false);
-        // console.log(data);
-        })
-  }, [])
 
   return (  
     <div className='home'>
-      {isPending && <div>Loading...</div>}
-      { blogs && <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title='All Blogs!'/> }
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Tiger')} title="Tiger's blogs" /> */}
 
     
