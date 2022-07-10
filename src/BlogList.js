4735a1592381c1863b91181fcc1f6a1c7450996b
Brit Wallace <britwallace888@gmail.com>
2022-07-10 16:11:36 -0700

    Add delete functionality

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs }) => {
   
 
   return (  
     <div className="blog-list">
-      <h2>{ title }</h2>
       {blogs.map((blog) => (
         <div className='blog-preview' key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
             <h2>{ blog.title }</h2>
-            <p>Written by { blog.author }</p>
+            <p>Written by blah { blog.author }</p>
             </Link>
           
         </div>
--- src/Create.js
+++ src/Create.js
@@ -1,16 +1,33 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
-  return (  
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      // history.go(-1);
+      history.push('/');
+    })
+  }
+
+  return (
     <div className="create">
-      <h2>Add a new Blog</h2>
-      <form>
-        <label>Blog title</label>
-        <input
-          type="text"
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
           required 
           value={title}
           onChange={(e) => setTitle(e.target.value)}
@@ -20,19 +37,16 @@ const Create = () => {
           required
           value={body}
           onChange={(e) => setBody(e.target.value)}
-        ></textarea>  
+        ></textarea>
         <label>Blog author:</label>
         <select
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         >
-          <option value="mario">Mario</option>
-          <option value="yoshi">Yoshi</option>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
-        <p>{ title }</p>
-        <p>{ body }</p>
-        <p>{ author }</p>
+        <button>Add Blog</button>
       </form>
     </div>
   );
